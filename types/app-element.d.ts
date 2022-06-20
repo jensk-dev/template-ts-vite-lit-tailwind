f6e0a96d420f4111930a99dbf80a7c09aa1196c5
Jens Kresse <contact@jensk.dev>
2022-06-20 20:19:51 +0200

    remove types

--- types/app-element.d.ts
+++
@@ -1,5 +0,0 @@
-import { TemplateResult } from "lit";
-import { TailwindElement } from "./components/tailwind-element/tailwind-element";
-export declare class RootElement extends TailwindElement {
-  protected render(): TemplateResult;
-}
