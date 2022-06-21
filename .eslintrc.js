3ce88279e1df5f2344035287afde63443f6fb431
Jens Kresse <contact@jensk.dev>
2022-06-22 00:17:52 +0200

    add eslint defaults

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "airbnb-base",
     "airbnb-typescript/base",
     "plugin:lit/recommended",
